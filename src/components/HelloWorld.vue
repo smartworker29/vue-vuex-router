2a12110dc9f99224acba8a53618dac26336e8ec3
soliddev312 <hitgoal312@gmail.com>
2017-11-02 03:19:37 -0500

    Add v-for
    Signed-off-by: soliddev312 <hitgoal312@gmail.com>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,19 +3,7 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for="user in users" v-bind:key="user" v-if="user.isShow" >{{ user.title }}</li>
     </ul>
   </div>
 </template>
@@ -28,7 +16,21 @@ export default {
   // }
   data () {
     return {
-      msg: 'Message from Carol at ' + new Date().toLocaleString()
+      msg: 'Message from Carol at ' + new Date().toLocaleString(),
+      users: [
+        {
+          isShow: true, 
+          title: 'A'
+        },
+        {
+          isShow: true, 
+          title: 'B'
+        },
+        {
+          isShow: false, 
+          title: 'C'
+        },
+      ]
     }
   }
 }
