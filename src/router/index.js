a007a2c636b054f31f4404b848c4abdc6ed2f409
soliddev312 <hitgoal312@gmail.com>
2017-12-11 06:38:29 -0500

    Update vue router
    Signed-off-by: soliddev312 <hitgoal312@gmail.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import HelloGround from '@/components/HelloGround'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-
-        { path: '/a', component: HelloWorld, alias: '/b' },
-        { path: '/b', component: HelloGround }
+    { path: '/b', component: HelloWorld }
   ]
 })
